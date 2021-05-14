dfcd13769c67dfeee3430d555fdc9f22b0b12bb0
Ni2Ko <724nkl@gmail.com>
2021-05-14 13:52:01 +0300

    ДЗ

--- Lesson_2/hw_1.py
+++ Lesson_2/hw_1.py
@@ -1,1 +1,5 @@
-print("Hello piople")
+list_1 = [1423423, 'Hi', 222.3, 1,'Hallo']
+a = 0
+while a <= len(list_1):
+    print(type(list_1[a]))
+    a = a + 1
--- Lesson_2/hw_2.py
+++ Lesson_2/hw_2.py
@@ -1,1 +1,17 @@
-print("Hay!")
+list_1 = input('Введите значения через пробел')
+list_1 = list_1.split(' ')
+print(list_1)
+lenth = len(list_1)
+a = 0
+b = 1
+if lenth % 2 == 0:
+    while b < lenth:
+        list_1[a], list_1[b] = list_1[b], list_1[a]
+        a = a + 2
+        b = b + 2
+else:
+    while a + 1 != lenth:
+        list_1[a], list_1[b] = list_1[b], list_1[a]
+        a = a + 2
+        b = b + 2
+print(list_1)
