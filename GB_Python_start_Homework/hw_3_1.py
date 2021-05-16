c3066fa334a7b8f0bbaae3cea76b7df2339a3f69
Ni2Ko <724nkl@gmail.com>
2021-05-16 23:39:04 +0300

    Задание 1

--- GB_Python_start_Homework/hw_3_1.py
+++ GB_Python_start_Homework/hw_3_1.py
@@ -1,2 +1,6 @@
-print('Homework 3')
-print('Test')
+num_1 = float(input('Введи первое число'))
+num_2 = float(input('Введи второе число'))
+if num_2 != 0:
+    print(round((lambda x, y: x / y)(num_1, num_2), 3))
+else:
+    print('Ты, что! На 0 делить нельзя!')
